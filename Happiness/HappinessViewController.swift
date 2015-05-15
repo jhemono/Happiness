a06275004578c7af02d8e4853e6d442e130f45f6
Julien Hémono <jhemono@me.com>
2015-05-15 14:17:49 +0200

    First Lecture

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Julien Hémono on 15/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+
+}
